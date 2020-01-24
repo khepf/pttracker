eca178de9d9bd4262ce21d72765f79f3bb9cb0c5
Jeremy Kepf <jeremy1975@gmail.com>
2020-01-23 21:22:26 -0500

    home page graphic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,18 @@
 <template>
   <v-container>
-    <v-layout text-center wrap>
-      <v-flex xs12>
+    <v-row>
+      <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('@/assets/ptguy.png')"
           class="my-3"
           contain
-          height="200"
+          height="500"
         ></v-img>
-      </v-flex>
+      </v-col>
+    </v-row>
 
-      <v-flex mb-4>
+    <v-row>
+      <v-col cols="12">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
@@ -21,56 +23,60 @@
             >Discord Community</a
           >
         </p>
-      </v-flex>
+      </v-col>
+    </v-row>
 
-      <v-flex mb-5 xs12>
+    <v-row>
+      <v-col cols="12">
         <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+      </v-col>
+    </v-row>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+    <v-row>
+      <v-col cols="12">
+        <a
+          v-for="(next, i) in whatsNext"
+          :key="i"
+          :href="next.href"
+          class="subheading mx-3"
+          target="_blank"
+        >
+          {{ next.text }}
+        </a>
+      </v-col>
+    </v-row>
 
-      <v-flex xs12 mb-5>
+    <v-row>
+      <v-col cols="12">
         <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+        <a
+          v-for="(link, i) in importantLinks"
+          :key="i"
+          :href="link.href"
+          class="subheading mx-3"
+          target="_blank"
+        >
+          {{ link.text }}
+        </a>
+      </v-col>
+    </v-row>
 
-      <v-flex xs12 mb-5>
+    <v-row>
+      <v-col cols="12">
         <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+        <a
+          v-for="(eco, i) in ecosystem"
+          :key="i"
+          :href="eco.href"
+          class="subheading mx-3"
+          target="_blank"
+        >
+          {{ eco.text }}
+        </a>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
--- src/views/LoginCard.vue
+++ src/views/LoginCard.vue
@@ -1,27 +1,38 @@
 <template>
-  <v-card width="400px" class="mt-5 mx-a">
-    <v-card-title class="pb-0">
-      <h1>Login</h1>
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-        <v-text-field
-          :type="showPassword ? 'text' : 'password'"
-          label="Password"
-          prepend-icon="mdi-lock"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword = !showPassword"
-        />
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-btn color="success">Register</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn color="info">Login</v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-container>
+    
+      <v-row :align="alignment" :justify="justify">
+        <v-col cols="12">
+        <v-card width="400px" class="mx-auto mt-5">
+          <v-card-title class="justify-center">
+            <h1>Log In</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Username"
+                prepend-icon="mdi-account-circle"
+              />
+              <v-text-field
+                :type="showPassword ? 'text' : 'password'"
+                label="Password"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+              />
+            </v-form>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="info">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+        </v-col>
+      </v-row>
+    
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -34,3 +45,4 @@ export default {
   }
 };
 </script>
+
